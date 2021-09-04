016d7ab2e2d48e5a2c4059f4e27cd450eb16fb6d
Bolajoko Olayinka Luqman <luksyb@gmail.com>
2021-09-04 13:32:33 +0100

    Update app.module.ts
    moved config out

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,7 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 @Module({
-  imports: [
-    MikroOrmModule.forRoot({
-      entities: ['dist/**/*.entity.js'],
-      entitiesTs: ['src/**/*.entity.ts'],
-      debug: true,
-      dbName: 'nest-mikro.sqlite3',
-      type: 'sqlite',
-    }),
-    UserModule,
-  ],
+  imports: [MikroOrmModule.forRoot(), UserModule],
   controllers: [],
   providers: [],
 })
