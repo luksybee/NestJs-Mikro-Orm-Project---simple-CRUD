91e6d4df2a336a20de0353e46c37bc75443a6816
Bolajoko Olayinka Luqman <luksyb@gmail.com>
2021-09-04 06:43:48 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     MikroOrmModule.forRoot({
-      entities: ['./dist/entities'],
-      entitiesTs: ['./src/entities'],
+      entities: ['dist/**/*.entity.js'],
+      entitiesTs: ['src/**/*.entity.ts'],
+      debug: true,
       dbName: 'nest-mikro.sqlite3',
       type: 'sqlite',
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
