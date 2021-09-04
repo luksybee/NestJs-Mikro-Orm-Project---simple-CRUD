a31d53d3d81ba94e20dca83a74ef92cfe80e5cf1
Bolajoko Olayinka Luqman <luksyb@gmail.com>
2021-09-04 13:33:08 +0100

    Update create-user.dto.ts
    added deccorators for swagger doc

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,17 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password: string;
 
+  @ApiProperty()
   profile_image: string;
 }
