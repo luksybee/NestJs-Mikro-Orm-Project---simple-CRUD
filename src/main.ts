986aa98343f99139b53eabbaf8284a1b3709ec34
Bolajoko Olayinka Luqman <luksyb@gmail.com>
2021-09-04 06:52:40 +0100

    Added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJs-Mikro-Orm-Project---simple-CRUD API')
+    .setDescription('NestJs-Mikro-Orm-Project---simple-CRUD REST API Specs.')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
